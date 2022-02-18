1fa099f5b2dd9785f5521e94e11105cda384dd4c
izmrui2020 <rui.iizumi.20@gmail.com>
2022-02-18 21:49:16 +0900

    first commit

---
+++ sequential_update/constantry/src/update.rs
@@ -0,0 +1,35 @@
+//
+use anyhow::Result;
+
+
+#[async_trait::async_trait]
+pub trait Instance {
+    async fn init(&mut self) -> Result<()>;
+    async fn update(&mut self) -> Result<()>;
+    async fn specific_function(&mut self) -> Result<()>;
+}
+
+pub struct Manager<T> {
+    instance: T,
+}
+
+impl<T> Manager<T>
+where
+    T: Send + Instance + 'static
+{
+    pub fn new(instance: T) -> Self {
+        Self {
+            instance
+        }
+    }
+
+    pub async fn create_manager(&mut self) -> Result<()> {
+        self.instance.init().await?;
+        Ok(())
+    }
+
+    pub async fn update_manager(&mut self) -> Result<()> {
+        
+        Ok(())
+    }
+}
